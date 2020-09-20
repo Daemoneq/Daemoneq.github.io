ba7ba3d4d408adc78dd93a56692575dedc1a8df3
Daemoneq <daniel.stepniak.99@gmail.com>
2020-09-20 20:02:26 +0200

    add content to second screen / add third screen with content

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const sideBar = document.querySelector(".sideBar")
 const darkArea = document.querySelector(".darkArea")
 const sideBarMenu = document.querySelector(".sideBarMenu")
 const sideBar__icon = document.querySelector(".sideBar__icon")
+const productsBar__button = document.querySelector(".productsBar__button")
+const productsBar__list = document.querySelector(".productsBar__list")
+
+
+let sideBarCounter = 0;
+function handleProductsBar() {
+    if (sideBarCounter % 2 === 0) {
+        productsBar__list.classList.add("productsBar__list--active")
+    } else {
+        productsBar__list.classList.remove("productsBar__list--active")
+    }
+    sideBarCounter += 1;
+}
+
+productsBar__button.addEventListener("click", handleProductsBar)
 
 
 let counter = 0;
@@ -23,6 +38,7 @@ function x() {
 function backSideBar() {
     sideBar.classList.remove("sideBar--active");
 }
+
 
 darkArea.addEventListener("click", x)
 sideBar.addEventListener("click", x)
