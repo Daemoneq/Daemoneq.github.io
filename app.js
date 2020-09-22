6a3d096e0217f9e8c524a74f78a4dc2b9a9ca4a2
Daemoneq <daniel.stepniak.99@gmail.com>
2020-09-22 22:17:13 +0200

    fix slider on fourth screen with js

--- app.js
+++ app.js
@@ -46,3 +46,48 @@ sideBar.addEventListener("click", x)
 
 // fourthScreenSlider
 
+const promotions__navButton1 = document.querySelector(".promotions__navButton1");
+const promotions__navButton2 = document.querySelector(".promotions__navButton2");
+const promotions__navButton3 = document.querySelector(".promotions__navButton3");
+
+const promoSliderTopSide = document.querySelector(".promoSliderTopSide");
+const promoSliderIcons = document.querySelector(".promoSliderIcons");
+
+function handleButton1() {
+    promoSliderTopSide.classList.add("margin1")
+    promoSliderTopSide.classList.remove("margin2")
+    promoSliderTopSide.classList.remove("margin3")
+    promoSliderIcons.classList.add("margin1")
+    promoSliderIcons.classList.remove("margin2")
+    promoSliderIcons.classList.remove("margin3")
+    promotions__navButton1.classList.add("promotions__navButton--active")
+    promotions__navButton2.classList.remove("promotions__navButton--active")
+    promotions__navButton3.classList.remove("promotions__navButton--active")
+}
+function handleButton2() {
+    promoSliderTopSide.classList.add("margin2")
+    promoSliderTopSide.classList.remove("margin1")
+    promoSliderTopSide.classList.remove("margin3")
+    promoSliderIcons.classList.add("margin2")
+    promoSliderIcons.classList.remove("margin1")
+    promoSliderIcons.classList.remove("margin3")
+    promotions__navButton1.classList.remove("promotions__navButton--active")
+    promotions__navButton2.classList.add("promotions__navButton--active")
+    promotions__navButton3.classList.remove("promotions__navButton--active")
+}
+function handleButton3() {
+    promoSliderTopSide.classList.add("margin3")
+    promoSliderTopSide.classList.remove("margin1")
+    promoSliderTopSide.classList.remove("margin2")
+    promoSliderIcons.classList.add("margin3")
+    promoSliderIcons.classList.remove("margin1")
+    promoSliderIcons.classList.remove("margin2")
+    promotions__navButton1.classList.remove("promotions__navButton--active")
+    promotions__navButton2.classList.remove("promotions__navButton--active")
+    promotions__navButton3.classList.add("promotions__navButton--active")
+}
+
+
+promotions__navButton1.addEventListener("click", handleButton1)
+promotions__navButton2.addEventListener("click", handleButton2)
+promotions__navButton3.addEventListener("click", handleButton3)
